f8415ad28e53f2c9423a8ec09d3a9966825eb599
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-10-14 16:48:00 -0700

    Add to the syntax highlighting query

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,25 +1,45 @@
 ; Identifier conventions
 
+; Assume all-caps names are constants
 ((identifier) @constant
  (match? @constant "^[A-Z][A-Z\\d_]+$'"))
 
+; Assume that uppercase names in paths are types
+((scoped_identifier
+  path: (identifier) @type)
+ (match? @type "^[A-Z]"))
+((scoped_identifier
+  path: (scoped_identifier
+    name: (identifier) @type))
+ (match? @type "^[A-Z]"))
+
+; Assume other uppercase names are enum constructors
 ((identifier) @constructor
  (match? @constructor "^[A-Z]"))
 
 ; Function calls
 
 (call_expression
-  (identifier) @function)
+  function: (identifier) @function)
 (call_expression
-  (field_expression (field_identifier) @function.method))
+  function: (field_expression
+    field: (field_identifier) @function.method))
 (call_expression
-  (scoped_identifier
+  function: (scoped_identifier
     "::"
-    (identifier) @function))
+    name: (identifier) @function))
 
+(generic_function
+  function: (identifier) @function)
+(generic_function
+  function: (scoped_identifier
+    name: (identifier) @function))
+(generic_function
+  function: (field_expression
+    field: (field_identifier) @function.method))
 
 (macro_invocation
-  (identifier) @function.macro
+  macro: (identifier) @function.macro
   "!" @function.macro)
 
 ; Function definitions
