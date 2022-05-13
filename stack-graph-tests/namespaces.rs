7b9c715e11745d331a43bb9d23ef541d26be9705
Jason Orendorff <jason.orendorff@gmail.com>
2022-05-13 08:27:10 -0500

    Non-working tests.

---
+++ stack-graph-tests/namespaces.rs
@@ -0,0 +1,16 @@
+pub struct Point {
+    pub x: u32,
+    pub y: u32
+}
+
+#[allow(non_snake_case)]
+pub fn Point(x: u32, y: u32) -> Point {
+    // ^ defined: 7
+    //                          ^ defined: 1
+    let p = Point { x, y };
+    //      ^ defined: 1
+    //              ^ defined: 2, 7
+    //                 ^ defined: 3, 7
+    p
+//  ^ defined: 10
+}
---
+++ stack-graph-tests/types.rs
@@ -0,0 +1,42 @@
+pub struct Frob;
+
+pub struct Point(u32, u32);
+
+// pub struct A {
+//     x: u32,
+//     y: Point,
+//     // ^ defined: 3
+//     next: Option<Box<A>>,
+//     //               ^ defined: 5
+// }
+//
+// pub struct Parser<'a> {
+//     source: &'a str,
+//     //       ^ defined: 13
+//     point: usize,
+// }
+//
+// pub enum MyResult<T, E> {
+//     Ok(T),
+//     // ^ defined: 19
+//     Err {
+//         err: E,
+//         //   ^ defined: 19
+//         blame: String,
+//         more_issues: Vec<E>,
+//         //               ^ defined: 19
+//     },
+//     Unsure,
+// }
+
+pub const FROB: Frob = Frob;
+//              ^ defined: 1
+//                     ^ defined: 1
+
+pub const ORIGIN: Point = Point(0, 0);
+//                ^ defined: 3
+//                        ^ defined: 3
+
+pub const POINT_FN: fn(u32, u32) -> Point = Point;
+//                                  ^ defined: 3
+//                                          ^ defined: 3
