1fee54d4eb6f4bfc45ee4b8b88b704e2e43c4f22
Jason Orendorff <jason.orendorff@gmail.com>
2022-05-16 02:37:48 -0500

    Support const generics except the ambiguous case.
    The ambiguity is in GenericArgument context when the argument is a single
    identifier.

--- stack-graph-tests/shadow_generic.rs
+++ stack-graph-tests/shadow_generic.rs
@@ -1,16 +1,22 @@
 pub struct T;
 
-pub struct V<T> {
+pub const K: usize = 0;
+
+pub struct V<T, const K: usize> {
     pub t: T,
-    //     ^ defined: 3
-    pub u: Box<T>,
-    //         ^ defined: 3
+    //     ^ defined: 5
+    pub u: Box<[T; K]>,
+    //          ^ defined: 5
+    //             ^ defined: 5
+    pub v: Option<Box<V<T, {K}>>>, // TODO: it should also work without braces
+    //                  ^ defined: 5
+    //                      ^ defined: 5
 }
 
 pub fn f<T>(
     t: T
-    // ^ defined: 10
+    // ^ defined: 16
 ) -> T {
-//   ^ defined: 10
+//   ^ defined: 16
     t
 }
