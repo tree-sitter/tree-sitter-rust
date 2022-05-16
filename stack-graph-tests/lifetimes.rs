add57726ec5826dcda808098b46d6f2c9d6fb4dd
Jason Orendorff <jason.orendorff@gmail.com>
2022-05-16 03:56:24 -0500

    A few more basic lifetime tests.

--- stack-graph-tests/lifetimes.rs
+++ stack-graph-tests/lifetimes.rs
@@ -1,6 +1,31 @@
 pub struct Parser<'src> {
-    source: &'src str,
-    //       ^ defined: 1
-    point: usize,
+    pub source: &'src str,
+    //           ^ defined: 1
+    pub point: usize,
 }
 
+pub fn f<'a>(
+    table: &'a [(&'a str, Vec<u8>)],
+    //      ^ defined: 7
+    //            ^ defined: 7
+    name: &str
+) -> &'a [u8] {
+//    ^ defined: 7
+    for (tname, tvalue) in table {
+        if *tname == name {
+            return tvalue;
+        }
+    }
+    &[]
+}
+
+impl<'src>
+    Parser<'src>
+//         ^ defined: 22
+{
+    fn source(&'src self) -> &'src str {
+        //     ^ defined: 22
+        //                    ^ defined: 22
+        self.source
+    }
+}
