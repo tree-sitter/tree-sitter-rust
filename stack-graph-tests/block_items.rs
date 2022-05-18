16ad446f03515be57952d438cbaa2cf302978b34
Jason Orendorff <jason.orendorff@gmail.com>
2022-05-18 07:21:00 -0500

    Support items in block scope.

---
+++ stack-graph-tests/block_items.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+fn test() {
+    fn f() {}
+    struct Info;
+
+    {
+        f();
+    //  ^ defined: 9
+        fn f() -> Info { todo!(); }
+        //        ^ defined: 11
+        struct Info(String);
+    }
+}
