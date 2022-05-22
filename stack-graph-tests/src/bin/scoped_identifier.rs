3fcdbf50634a569bf0b0fa9b3ccc170342c6aa2d
Jason Orendorff <jason.orendorff@gmail.com>
2022-05-22 15:44:58 -0500

    Treat values like types, rather than hang them directly on scopes.
    This should make paths to const and static items work, as well as paths in
    the position marked by `(_)` in `(tuple_struct_pattern type: (_))` and
    `(struct_pattern type: (_))`. Incomplete tests.

--- stack-graph-tests/src/bin/scoped_identifier.rs
+++ stack-graph-tests/src/bin/scoped_identifier.rs
@@ -4,12 +4,25 @@ mod a {
             pub d: i32,
         }
     }
+
+    pub const ANSWER: i32 = 7;
 }
 
 fn main() {
-    let _ = a::b::C { d: 7 };
+    let x = a::b::C { d: 7 };
     //      ^ defined: 1
     //         ^ defined: 2
     //            ^ defined: 3
     //                ^ defined: 4
+
+    let a::b::C { d } = x;
+    //  ^ defined: 1
+    //     ^ defined: 2
+    //        ^ defined: 3
+    //            ^ defined: 4
+
+    let ok = d == a::ANSWER;
+    //            ^ defined: 1
+    //               ^ defined: 8
+    assert!(ok);
 }
