bde995593b39116a34f102b9190d9e17c0bdce30
Jason Orendorff <jason.orendorff@gmail.com>
2022-05-29 06:45:19 -0500

    Passing tests for qualified access to enum variants.

---
+++ stack-graph-tests/src/bin/scoped_identifier_variants.rs
@@ -0,0 +1,15 @@
+enum MyResult<T, E> {
+    Fine(T),
+    Nope(E),
+}
+
+pub fn f() -> MyResult<(), String> {
+    MyResult::Fine(())
+    //        ^ defined: 2
+}
+
+use MyResult::Fine;
+const ANOTHER: MyResult<&str, &str> = Fine("mess");
+//                                    ^ defined: 2
+
+fn main() {}
