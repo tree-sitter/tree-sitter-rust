9b8b8a5ca73702793d6d1fa7509732a9ee53f976
Jason Orendorff <jason.orendorff@gmail.com>
2022-07-06 11:48:24 -0500

    Handle more `use` syntax: aliases and scoped use-lists.
    Wildcards, leading `::`, `self` in imports, and unscoped use-lists are not
    supported yet.

--- stack-graph-tests/src/bin/use_declaration.rs
+++ stack-graph-tests/src/bin/use_declaration.rs
@@ -27,7 +27,7 @@ pub mod p {
     }
 }
 
-fn working() {
+fn basic_example() {
     use a::b::c;
     //  ^ defined: 5
     //     ^ defined: 6
@@ -36,15 +36,96 @@ fn working() {
 //  ^ defined: 7
 }
 
-fn unsupported() {
-    // Don't crash when unsupported imports are present.
-    // examples from the Rust reference
-    { use a::b::{c, d, e::f, g::h::i}; }
-    { use a::b::{self, c, d::e}; }
-    { use p::q::r as x; }
-    { use a::b::{self as ab, c as abc}; }
-    { use a::b::*; }
-    { use a::b::{self as ab, c, d::{*, e::f}}; }
+fn rust_reference_examples() {
+    // From <https://doc.rust-lang.org/reference/items/use-declarations.html>.
+
+    fn c() {}
+    fn e() {}
+    fn f() {}
+    fn i() {}
+
+    {
+        use a::b::{c, d, e::f, g::h::i};
+        //  ^ defined: 5
+        //     ^ defined: 6
+        //         ^ defined: 7
+        //            ^ defined: 8
+        //               ^ defined: 13
+        //                  ^ defined: 14
+        //                     ^ defined: 16
+        //                        ^ defined: 17
+        //                           ^ defined: 18
+        c();
+    //  ^ defined: 7
+        d::e::f();
+    //  ^ defined: 8
+        // ^ defined: 9
+        //    ^ defined: 10
+        f();
+    //  ^ defined: 14
+        i();
+    //  ^ defined: 18
+    }
+
+    {
+        use a::b::{self, c, d::e};
+        //  ^ defined: 5
+        //     ^ defined: 6
+        //         ^ defined: 6
+        //               ^ defined: 7
+        //                  ^ defined: 8
+        //                     ^ defined: 9
+        // TODO - support `self` in use-declarations
+        c();
+    //  ^ defined: 7
+        e::f();
+    //  ^ defined: 9
+        // ^ defined: 10
+    }
+
+    {
+        use p::q::r as x;
+        //  ^ defined: 24
+        //     ^ defined: 25
+        //        ^ defined: 26
+        x();
+    //  ^ defined: 26
+    }
+
+    {
+        use a::b::{self as ab, c as abc};
+        //  ^ defined: 5
+        //     ^ defined: 6
+        //         ^ defined: 6
+        //                     ^ defined: 7
+        // TODO - support aliased self import
+        abc();
+    //  ^ defined: 7
+    }
+
+    {
+        use a::b::*;
+        //  ^ defined: 5
+        //     ^ defined: 6
+        // TODO - support wildcard imports
+    }
+
+    {
+        use a::b::{self as ab, c, d::{*, e::f}};
+        //  ^ defined: 5
+        //     ^ defined: 6
+        //         ^ defined: 6
+        //                     ^ defined: 7
+        //                        ^ defined: 8
+        //                               ^ defined: 9
+        //                                  ^ defined: 10
+        // TODO - support aliased self import
+        c();
+    //  ^ defined: 7
+        // TODO - support wildcard imports
+        f();
+    //  ^ defined: 10
+    }
 }
 
 fn main() {}
