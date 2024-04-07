ae506b94d3341e9417cb1bcf2988cf7fcacb02c8
Amaan Qureshi <amaanq12@gmail.com>
2024-04-06 22:30:31 -0400

    build: update bindings and workflows

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,16 +1,17 @@
 fn main() {
     let src_dir = std::path::Path::new("src");
-    let mut config = cc::Build::new();
-    config.include(&src_dir);
-    config
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .flag_if_supported("-Wno-trigraphs");
+
+    let mut c_config = cc::Build::new();
+    c_config.flag("-Wno-unused-parameter");
+    c_config.std("c11").include(src_dir);
+
     let parser_path = src_dir.join("parser.c");
-    let scanner_path = src_dir.join("scanner.c");
+    c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+
+    let scanner_path = src_dir.join("scanner.c");
+    c_config.file(&scanner_path);
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    config.file(&parser_path);
-    config.file(&scanner_path);
-    config.compile("parser-scanner");
+
+    c_config.compile("tree-sitter-rust");
 }
