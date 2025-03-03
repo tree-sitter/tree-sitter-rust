cd277018716480d7aa1d1c671832ef98c29eaf27
may <m4rch3n1ng@gmail.com>
2025-03-03 02:50:57 +0100

    initialize parsing of frontmatter

--- grammar.js
+++ grammar.js
@@ -77,6 +77,7 @@ module.exports = grammar({
     $._inner_block_doc_comment_marker,
     $._block_comment_content,
     $._line_doc_content,
+    $.frontmatter,
     $._error_sentinel,
   ],
 
@@ -119,6 +120,7 @@ module.exports = grammar({
   rules: {
     source_file: $ => seq(
       optional($.shebang),
+      optional($.frontmatter),
       repeat($._statement),
     ),
 
