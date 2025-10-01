dd8980ba6fbba331777571703e2ab32f30a5f2c8
Vanessa Rodrigues <vanessa.rodrigues@microsoft.com>
2025-10-01 08:42:12 -0300

    Unsafe foreign_mod_item and CI updates

--- grammar.js
+++ grammar.js
@@ -110,6 +110,7 @@ module.exports = grammar({
     [$.array_expression],
     [$.visibility_modifier],
     [$.visibility_modifier, $.scoped_identifier, $.scoped_type_identifier],
+    [$.foreign_mod_item, $.function_modifiers],
   ],
 
   word: $ => $.identifier,
@@ -279,7 +280,7 @@ module.exports = grammar({
     ),
 
     foreign_mod_item: $ => seq(
-      optional($.visibility_modifier),
+      optional('unsafe'),
       $.extern_modifier,
       choice(
         ';',
